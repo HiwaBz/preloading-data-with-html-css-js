e06958d690dc5cfb3d890fbcd144fdf2ca37f91b
hiwaBz <hiwa.bzd@gmail.com>
2023-04-15 00:06:24 +0430

    load more button added

--- script.js
+++ script.js
@@ -3,47 +3,76 @@ const API_URL = 'http://'
 const boxContainer = document.querySelector('.box-container')
 const box = document.querySelectorAll('.box')
 const imgs = document.querySelectorAll('img')
+const btn = document.querySelector('.loadMoreBtn')
+const animatedLoading = document.querySelector('.loadingGif')
+let lmt = 1
 
 
 
-// getting data from API
+// * getting data from API :
 
-const getData = async function (limit = 4) {
-    const page = Math.ceil(Math.random() * 10) 
-    // console.log(page);
+const getData = async function (limit = lmt * 4) {
+    const page = Math.ceil(Math.random() * 10)
     const res = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
-    // console.log(await res.clone().json());
     return await res.clone().json()
 }
 
 
-// hiding the undefinded picture src :
 
-// const imagesDisplaySetting = function (dis) {
-//     document.addEventListener("DOMContentLoaded", function (event) {
-//         document.querySelectorAll('img').forEach(function (img) {
-//             img.style.display = `${dis}`;
-//         })
-//     });
-// }
-// implement the ui section : 
+// * implement the ui section : 
 
 const generateMarkup = async function () {
 
-    // display none before image load
+    const data = await getData(lmt * 4)
 
-    // imagesDisplaySetting('none')
-
-    const data = await getData()
-
-    box.forEach((el, i) => {
+    Array.from(boxContainer.querySelectorAll('.box')).slice(-4).forEach((el, i) => {
         el.querySelector('h4').textContent = data[i].author;
         el.querySelector('img').src = data[i].download_url;
 
-        // change the image display to block :
-        // el.querySelector('img').style.display = 'block';
-
+        // * change the image display to block :
         [...el.children].forEach(elem => elem.style.animation = "none")
+
+    })
+
+    // * remove loading animation :
+
+    animatedLoading.classList.add('hidden')
+}
+
+
+const addBox = function () {
+
+    btn.addEventListener('click', async () => {
+        animatedLoading.classList.remove('hidden')
+        const markup = `<div class="box">
+        <h4 style="animation: bounce ease-in-out infinite 1s;">Author</h4>
+        <img src="" alt="" style="animation: bounce ease-in-out infinite 1s;">
+    </div>
+
+    <div class="box">
+        <h4 style="animation: bounce ease-in-out infinite 1s;">Author</h4>
+        <img src="" alt="" style="animation: bounce ease-in-out infinite 1s;">
+    </div>
+
+    <div class="box">
+        <h4 style="animation: bounce ease-in-out infinite 1s;">Author</h4>
+        <img src="" alt="" style="animation: bounce ease-in-out infinite 1s;">
+    </div>
+
+    <div class="box">
+        <h4 style="animation: bounce ease-in-out infinite 1s;">Author</h4>
+        <img src="" alt="" style="animation: bounce ease-in-out infinite 1s;">
+    </div>`
+
+
+        boxContainer.insertAdjacentHTML('beforeend', markup)
+        // 
+        lmt++;
+        // ? generate markup after boxes inserted to the container : 
+        await generateMarkup();
+
+
+
 
     })
 }
@@ -52,6 +81,7 @@ const generateMarkup = async function () {
 const loadData = async function () {
 
     try {
+        addBox();
         await generateMarkup();
 
 
